1e9940ce13efdd4cd1e6abc017ba30a47285b6b4
cvuser0 <2580@live.nl>
2015-12-08 13:35:40 +0100

    post-initial-commit commit

--- www/SecondWebViewPlugin.js
+++ www/SecondWebViewPlugin.js
@@ -2,27 +2,27 @@
 module.exports = (function()
 {
     'use strict';
-
-    var _show = function( url, successCallback, errorCallback )
-    {
-        cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'show', [ url ] );
-    };
-
-    var _hide = function( successCallback, errorCallback )
-    {
-        cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'hide', [] );
-    };
-
-    var _subscribeCallback = function( successCallback, errorCallback )
-    {
-        cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'subscribeCallback', [] );
-    };
-
     return {
-        Show             : _show,
-        Hide             : _hide,
-        Close            : _hide,
-        SubscribeCallback: _subscribeCallback
+        Show             : function( url, successCallback, errorCallback )
+        {
+            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'show', [ url ] );
+        },
+        Hide             : function( successCallback, errorCallback )
+        {
+            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'hide', [] );
+        },
+        SubscribeCallback: function( successCallback, errorCallback )
+        {
+            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'subscribeCallback', [] );
+        },
+        Close            : this.Hide,
+        ExecuteCode      : function()
+        {
+            //TODO
+        },
+        ExecuteFile      : function()
+        {
+            //TODO
+        }
     };
-
 })();
