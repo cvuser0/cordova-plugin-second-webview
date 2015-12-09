f0760e467eb1e827f777b8e179675481f503f675
cvuser0 <2580@live.nl>
2015-12-09 15:56:08 +0100

    finally discovered the auto-format key in my IDE (reformatted code) and i added some stuff to help me understand what is going on 'in there'

--- src/android/org/apache/cordova/secondwebview/SecondWebViewPlugin.java
+++ src/android/org/apache/cordova/secondwebview/SecondWebViewPlugin.java
@@ -34,122 +34,130 @@ import org.apache.cordova.LOG;
 import org.apache.cordova.engine.*;
 
 
-
 public class SecondWebViewPlugin extends CordovaPlugin {
 
-  private static final String LOG_TAG = "SecondWebview";
-  private CallbackContext closeCallback;
+    private static final String LOG_TAG = "SecondWebview";
+    private CallbackContext closeCallback;
+    private customCallback ctpCallback = null; //Child To Parent
+    private customCallback ptcCallback = null; //Parent To Child
 
-  public SecondWebViewPlugin() {
-
-  }
-
-  /**
-  * Sets the context of the Command. This can then be used to do things like
-  * get file paths associated with the Activity.
-  *
-  * @param cordova
-  *            The context of the main Activity.
-  * @param webView
-  *            The CordovaWebView Cordova is running in.
-  */
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-  }
-
-  /**
-  * Executes the request and returns PluginResult.
-  *
-  * @param action
-  *            The action to execute.
-  * @param args
-  *            JSONArry of arguments for the plugin.
-  * @param callbackContext
-  *            The callback id used when calling back into JavaScript.
-  * @return True if the action was valid, false if not.
-  */
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("show") && args.length() > 0) {
-      LOG.d(LOG_TAG, "Show Second Webview");
-      final String url = args.getString(0);
-
-      if(!"".equals(url)) {
-        showWebView(url);
-        JSONObject r = new JSONObject();
-        r.put("responseCode", "ok");
-        callbackContext.success(r);
-      }
-      else {
-        callbackContext.error("Empty Parameter url");
-      }
+    public SecondWebViewPlugin() {
 
     }
-    else if(action.equals("hide")) {
-      LOG.d(LOG_TAG, "Hide Second Webview");
-      hideWebView();
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-    else if(action.equals("injectJSCode")){
-      //TODO inject JS Code
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-    else if(action.equals("injectJSFile")){
-      //TODO inject JS File
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-    else if(action.equals("injectCSSCode")){
-      //TODO inject CSS Code
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-    else if(action.equals("injectCSSFile")){
-      //TODO inject CSS File
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-    else if(action.equals("registerReceiver")){
-      //TODO register JS Function Callback
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-    else if(action.equals("sendData")){
-      //TODO inject JS Function Call
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
     }
 
-    else {
-      return false;
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action          The action to execute.
+     * @param args            JSONArry of arguments for the plugin.
+     * @param callbackContext The callback id used when calling back into JavaScript.
+     * @return True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("show") && args.length() > 0) {
+            LOG.d(LOG_TAG, "Show Second Webview");
+            final String url = args.getString(0);
+            if (!"".equals(url)) {
+                showWebView(url);
+                JSONObject r = new JSONObject();
+                r.put("responseCode", "ok");
+                callbackContext.success(r);
+            } else {
+                callbackContext.error("Empty Parameter url");
+            }
+
+        } // end show
+        else if (action.equals("hide")) {
+            LOG.d(LOG_TAG, "Hide Second Webview");
+            hideWebView();
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            callbackContext.success(r);
+        } // end hide
+        else if (action.equals("injectJSCode")) {
+            //TODO inject JS Code
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            callbackContext.success(r);
+        } // end injectJSCode
+        else if (action.equals("injectJSFile")) {
+            //TODO inject JS File
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            callbackContext.success(r);
+        } //end injectJSFile
+        else if (action.equals("injectCSSFile")) {
+            //TODO inject CSS File
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            callbackContext.success(r);
+        } // end injectCSSFile
+        else if (action.equals("registerReceiver")) {
+            //TODO register JS Function Callback
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+
+            ctpCallback = new customCallback() {
+                public void call() {
+                    callbackContext.success(); //use first callback as callback for webview
+                }
+            }
+            callbackContext.fail(r); //use second callback as success and fail
+        } // end registerReceiver
+        else if (action.equals("sendData")) {
+            //TODO inject JS Function Call
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            ptcCallback = new customCallback() {
+                public void call() {
+                    callbackContext.success(); //use first callback as return from webview
+                }
+            }
+            callbackContext.fail(r); //use second callback as success and fail
+        } // end sendData
+        else if (action.equals("getJSONArray")) {
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            r.put("args", args);
+            callbackContext.success(r);
+        } // end getJSONArray
+        else {
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-  }
+    private void loadURL(String url) {
+        //TODO get second webview and call loadUrl on it
+    }
 
-  private void loadURL(String url)
-  {
-    //TODO get second webview and call loadUrl on it
-  }
+    private void showWebView(final String url) {
+        LOG.d(LOG_TAG, "Url: " + url);
+        Intent i = new Intent(this.cordova.getActivity(), SecondWebViewActivity.class);
+        i.putExtra("url", url);
+        i.putExtra("callback", )
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.cordova.getActivity().getApplicationContext().startActivity(i);
+    }
 
-  private void showWebView(final String url) {
-    LOG.d(LOG_TAG, "Url: " + url);
-    Intent i = new Intent(this.cordova.getActivity(), SecondWebViewActivity.class);
-    i.putExtra("url", url);
-    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    this.cordova.getActivity().getApplicationContext().startActivity(i);
-  }
+    private void hideWebView() {
+        LOG.d(LOG_TAG, "hideWebView");
+        this.cordova.getActivity().finish();
+    }
 
-  private void hideWebView() {
-    LOG.d(LOG_TAG, "hideWebView");
-    this.cordova.getActivity().finish();
-  }
+    public interface customCallback {
+        void call();
+    }
 }
--- www/SecondWebViewPlugin.js
+++ www/SecondWebViewPlugin.js
@@ -1,47 +1,36 @@
 /*global elu, module */
-module.exports = (function()
-{
+module.exports = (function () {
     'use strict';
     return {
-        Show             : function( url, successCallback, errorCallback )
-        {
-            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'show', [ url ] );
+        Show: function (url, successCallback, errorCallback) {
+            cordova.exec(successCallback, errorCallback, 'SecondWebViewPlugin', 'show', [url]);
         },
-        Hide             : function( successCallback, errorCallback )
-        {
-            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'hide', [] );
+        Hide: function (successCallback, errorCallback) {
+            cordova.exec(successCallback, errorCallback, 'SecondWebViewPlugin', 'hide', []);
         },
-        SubscribeCallback: function( successCallback, errorCallback )
-        {
-            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'subscribeCallback', [] );
+        SubscribeCallback: function (successCallback, errorCallback) {
+            cordova.exec(successCallback, errorCallback, 'SecondWebViewPlugin', 'subscribeCallback', []);
         },
-        Close            : function( successCallback, errorCallback )
-        {
-            cordova.exec( successCallback, errorCallback, 'SecondWebViewPlugin', 'hide', [] );
+        Close: function (successCallback, errorCallback) {
+            cordova.exec(successCallback, errorCallback, 'SecondWebViewPlugin', 'hide', []);
         },
-        InjectJSCode     : function( code, callback )
-        {
-            cordova.exec( callback, callback, 'SecondWebViewPlugin', 'injectJSCode', [ code, !!callback ] );
+        InjectJSCode: function (code, callback) {
+            cordova.exec(callback, callback, 'SecondWebViewPlugin', 'injectJSCode', [code, !!callback]);
         },
-        InjectJSFile     : function( file, callback )
-        {
-            cordova.exec( callback, callback, 'SecondWebViewPlugin', 'injectJSFile', [ file, !!callback ] );
+        InjectJSFile: function (file, callback) {
+            cordova.exec(callback, callback, 'SecondWebViewPlugin', 'injectJSFile', [file, !!callback]);
         },
-        InjectCSSCode    : function( code, callback )
-        {
-            cordova.exec( callback, callback, 'SecondWebViewPlugin', 'injectCSSCode', [ code, !!callback ] );
+        InjectCSSFile: function (file, callback) {
+            cordova.exec(callback, callback, 'SecondWebViewPlugin', 'injectCSSFile', [file, !!callback]);
         },
-        InjectCSSFile    : function( file, callback )
-        {
-            cordova.exec( callback, callback, 'SecondWebViewPlugin', 'injectCSSFile', [ file, !!callback ] );
+        RegisterReceiver: function (callback, receiver) {
+            cordova.exec(callback, callback, 'SecondWebViewPlugin', 'registerReceiver', [receiver]);
         },
-        RegisterReceiver : function( callback, receiver )
-        {
-            cordova.exec( callback, callback, 'SecondWebViewPlugin', 'registerReceiver', [ receiver ] );
+        SendData: function (callback, data) {
+            cordova.exec(callback, callback, 'SecondWebViewPlugin', 'sendData', [data]);
         },
-        Send             : function( callback, data )
-        {
-            cordova.exec( callback, callback, 'SecondWebViewPlugin', 'send', [ data ] );
+        GetJSONArray: function (callback, args) {
+            cordova.exec(callback, callback, 'SecondWebViewPlugin', 'getJSONArray', [args]);
         }
     };
 })();
