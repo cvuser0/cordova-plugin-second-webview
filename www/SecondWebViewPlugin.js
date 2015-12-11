443933785a465024598d453a3d66b4c095a1470b
cvuser0 <2580@live.nl>
2015-12-11 12:16:00 +0100

    Changes

--- www/SecondWebViewPlugin.js
+++ www/SecondWebViewPlugin.js
@@ -1,28 +1,29 @@
 module.exports = (function () {
     'use strict';
     return {
-        _plugin: 'SecondWebViewPlugin',
-        _action: {
-            open: 'open',
-            closeChild: 'closeChild',
-            closeParent: 'closeParent',
-            registerPingReceiver: 'registerPingReceiver',
-            ping: 'ping'
-        },
-        Open: function (url, pass, fail) {
-            cordova.exec(pass, fail, this._plugin, this._action.open, [url]);
-        },
-        CloseChild: function (pass, fail) {
-            cordova.exec(pass, fail, this._plugin, this._action.closeChild, []);
-        },
-        CloseParent: function (pass, fail) {
-            cordova.exec(pass, fail, this._plugin, this._action.closeParent, []);
-        },
-        RegisterPingReceiver: function (receiver, pass, fail) {
-            cordova.exec(pass, fail, this._plugin, this._action.registerPingReceiver, [receiver]);
-        },
-        Ping: function (data, isBased, pass, fail) {
-            cordova.exec(pass, fail, this._plugin, this._action.ping, [{data: data, isBased: isBased}]);
+        _plugin              : 'SecondWebViewPlugin' ,
+        _action              : {
+            open                 : 'open' ,
+            closeChild           : 'closeChild' ,
+            closeParent          : 'closeParent' ,
+            registerPingReceiver : 'registerPingReceiver' ,
+            ping                 : 'ping'
+        } ,
+        Open                 : function ( url , pass , fail ) {
+            cordova.exec ( pass , fail , this._plugin , this._action.open , [ url ] );
+        } ,
+        CloseChild           : function ( pass , fail ) {
+            cordova.exec ( pass , fail , this._plugin , this._action.closeChild , [] );
+        } ,
+        CloseParent          : function ( pass , fail ) {
+            cordova.exec ( pass , fail , this._plugin , this._action.closeParent , [] );
+        } ,
+        RegisterPingReceiver : function ( receiver , pass , fail ) {
+            cordova.exec ( pass , fail , this._plugin , this._action.registerPingReceiver , [ receiver ] );
+        } ,
+        Ping                 : function ( data , isBased , pass , fail ) {
+            cordova.exec ( pass , fail , this._plugin , this._action.ping , [ { data : data , isBased : isBased } ] );
         }
     };
-})();
+}
+) ();
