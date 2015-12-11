d52175b47861a24aca0d8eced77a68dd7e132c05
cvuser0 <cvuser0@users.noreply.github.com>
2015-12-11 09:45:33 +0100

    Realized Cordova wraps this with cordova.define automatically... -_-

--- www/SecondWebViewPlugin.js
+++ www/SecondWebViewPlugin.js
@@ -1,29 +1,27 @@
-cordova.define( "cordova-plugin-second-webview.webview" , function ( require , exports , module ) {
-    module.exports = (function () {
-        'use strict';
-        return {
-            _plugin :              'SecondWebViewPlugin' ,
-            _action :              {
-                open :                 'open' ,
-                close :                'close' ,
-                registerPingReceiver : 'registerPingReceiver' ,
-                ping :                 'ping'
-            } ,
-            Open :                 function ( url , pass , fail ) {
-                cordova.exec( pass , fail , this._plugin , this._action.open , [ url ] );
-            } ,
-            Close :                function ( pass , fail ) {
-                cordova.exec( pass , fail , this._plugin , this._action.close , [] );
-            } ,
-            RegisterPingReceiver : function ( receiver , pass , fail ) {
-                cordova.exec( pass , fail , this._plugin , this._action.registerPingReceiver , [ receiver ] );
-            } ,
-            Ping :                 function ( data , isBased , pass , fail ) {
-                cordova.exec( pass , fail , this._plugin , this._action.ping , [ {
-                    data :    data ,
-                    isBased : isBased
-                } ] );
-            }
-        };
-    }) ();
-} );
+module.exports = (function () {
+    'use strict';
+    return {
+        _plugin :              'SecondWebViewPlugin' ,
+        _action :              {
+            open :                 'open' ,
+            close :                'close' ,
+            registerPingReceiver : 'registerPingReceiver' ,
+            ping :                 'ping'
+        } ,
+        Open :                 function ( url , pass , fail ) {
+            cordova.exec( pass , fail , this._plugin , this._action.open , [ url ] );
+        } ,
+        Close :                function ( pass , fail ) {
+            cordova.exec( pass , fail , this._plugin , this._action.close , [] );
+        } ,
+        RegisterPingReceiver : function ( receiver , pass , fail ) {
+            cordova.exec( pass , fail , this._plugin , this._action.registerPingReceiver , [ receiver ] );
+        } ,
+        Ping :                 function ( data , isBased , pass , fail ) {
+            cordova.exec( pass , fail , this._plugin , this._action.ping , [ {
+                data :    data ,
+                isBased : isBased
+            } ] );
+        }
+    };
+}) ();
